79e9719fa3e610484995677257e75346466c72f3
ShashankAlampally <118708017+ShashankAlampally@users.noreply.github.com>
2024-05-15 16:10:40 +0530

    Update data.js

--- Constants/data.js
+++ Constants/data.js
@@ -2,4 +2,4 @@ exports.data = [["A",10.28,"LED Screen","Screen"],["B",24.07,"OLED Screen","Scre
             ["D",25.94,"Wide-Angle Camera","Camera"],["E",32.39,"Ultra-Wide-Angle Camera","Camera"],["F",18.77,"USE-C Port","Port"],
             ["G",15.13,"Micro-USB Port","Port"],["H",20.00,"Lightning Port","Port"],["I",42.31,"Andriod OS","OS"],["J",45.00,"iOS OS","OS"],
             ["K",45.00,"Metallic Body","Body"],["L",30.00,"Plastic Body","Body"]];
-exports.dataCount = [3,2,3,2,2];
+
